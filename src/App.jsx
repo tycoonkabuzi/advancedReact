6c42274ab5473bcd26e601cf76f5eb49cb21fe2d
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-01-28 17:50:11 +0100

    Understanding use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const addValue = () => {
+    setCount(count + 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+  const showInConsole = () => {
+    console.log(count);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1> Hook -useRef</h1>
+      <p>The count is :{count}</p>
+      <button onClick={addValue}>Add</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={showInConsole}>Show in console</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
